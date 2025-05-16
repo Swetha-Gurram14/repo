7a9a897bc45ee32727bfc6a4af9a5687c68490a7
Swetha-Gurram14 <swethagurram65@gmail.com>
2025-05-16 16:18:50 +0530

    first code

---
+++ New folder/program16.py
@@ -0,0 +1,10 @@
+class student:
+    def __int__(self,sname,sage,saddrr):
+        self.name=sname
+        self.age=sage
+        self.addrr=saddrr
+s1= student("swetha",22,"guntur")
+s2= student("ramya",22,"guntur")
+print(s1.age)
+print(s2.name)
+print(s1.age,s1.name,s1.addrr)
